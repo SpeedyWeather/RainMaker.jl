f34d50473310041c8e76f639956af30dd501c436
Milan <milankloewer@gmx.de>
2024-10-08 15:45:12 +0100

    remove build md files

--- docs/make.jl
+++ docs/make.jl
@@ -4,21 +4,42 @@ using Documenter
 
 DocMeta.setdocmeta!(RainMaker, :DocTestSetup, :(using RainMaker); recursive=true)
 
+# GET ALL SUBMISSIONS
+submissions = filter(x -> endswith(x, ".jl"), readdir("../submissions"))
+sort!(submissions)  # alphabetical order
+
+submissions_dict = Dict{String, Dict}()
+for submission in submissions
+    @info "Building submission $submission"
+    name = split(submission, ".jl")[1]
+    path = joinpath(@__DIR__, "..", "submissions", submission)
+    code = read(path, String)
+    submission_dict = Dict(
+        "path" => path,
+        "code" => code,
+    )
+    submissions_dict[name] = submission_dict
+end
+
 # GENERATE SUBMISSIONS
 @info "Building submissions.md"
 open(joinpath(@__DIR__, "src/submissions.md"), "w") do mdfile
-    header = read(joinpath(@__DIR__, "src/submissions_header.md"), String)
+    header = read(joinpath(@__DIR__, "headers/submissions_header.md"), String)
     println(mdfile, header)
-    println(mdfile, "## Submission 1")
-    println(mdfile, "\nsomething\n")
-    println(mdfile, "## Submission 2")
-    println(mdfile, "\nsomething else\n")
+
+    for (name, dict) in submissions_dict
+        println(mdfile, "## $name\n")
+        println(mdfile, "path: /submissions/$name.jl\n")
+        println(mdfile, "```@example $name")
+        println(mdfile, dict["code"])
+        println(mdfile, "```\n")
+    end
 end
 
 # GENERATE LEADERBOARD
 @info "Building leaderboard.md"
 open(joinpath(@__DIR__, "src/leaderboard.md"), "w") do mdfile
-    header = read(joinpath(@__DIR__, "src/leaderboard_header.md"), String)
+    header = read(joinpath(@__DIR__, "headers/leaderboard_header.md"), String)
     println(mdfile, header)
     println(mdfile, "| Milan | Test | 1,1 | 0 | 50 | 1 |")
 end
