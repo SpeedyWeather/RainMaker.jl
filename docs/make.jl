e149b56b3ce9a963880f0da4300c18522a69a8ae
Milan <milankloewer@gmx.de>
2024-10-08 16:16:05 +0100

    leaderboard first version without sorting

--- docs/make.jl
+++ docs/make.jl
@@ -1,11 +1,13 @@
 using SpeedyWeather
 using RainMaker
 using Documenter
+using Printf
+using Dates
 
 DocMeta.setdocmeta!(RainMaker, :DocTestSetup, :(using RainMaker); recursive=true)
 
-# GET ALL SUBMISSIONS
-submissions = filter(x -> endswith(x, ".jl"), readdir("../submissions"))
+# READ ALL SUBMISSIONS CODE
+submissions = filter(x -> endswith(x, ".jl"), joinpath(@__DIR__, "../submissions"))
 sort!(submissions)  # alphabetical order
 
 submissions_dict = Dict{String, Dict}()
@@ -21,7 +23,7 @@ for submission in submissions
     submissions_dict[name] = submission_dict
 end
 
-# GENERATE SUBMISSIONS
+# GENERATE SUBMISSIONS LIST
 @info "Building submissions.md"
 open(joinpath(@__DIR__, "src/submissions.md"), "w") do mdfile
     header = read(joinpath(@__DIR__, "headers/submissions_header.md"), String)
@@ -36,12 +38,44 @@ open(joinpath(@__DIR__, "src/submissions.md"), "w") do mdfile
     end
 end
 
+# RUN SUBMISSIONS
+function run_submission(path::String)
+    
+    # run submission
+    include(path)
+
+    # analyse/evaluate
+    total_precip = maximum(rain_gauge.accumulated_rain_large_scale) + maximum(rain_gauge.accumulated_rain_convection)
+    eval_dict = Dict(
+        "name" => team_name,
+        "description" => description,
+        "location" => (rain_gauge.lond, rain_gauge.latd),
+        "total precipitation" => total_precip,
+        "convection share" => maximum(rain_gauge.accumulated_rain_convection) / total_precip,
+        "period" => rain_gauge.measurement_counter*rain_gauge.Δt,
+    )
+    return eval_dict
+end
+
+
 # GENERATE LEADERBOARD
 @info "Building leaderboard.md"
 open(joinpath(@__DIR__, "src/leaderboard.md"), "w") do mdfile
     header = read(joinpath(@__DIR__, "headers/leaderboard_header.md"), String)
     println(mdfile, header)
-    println(mdfile, "| Milan | Test | 1,1 | 0 | 50 | 1 |")
+    for (name, dict) in submissions_dict
+        eval_dict = run_submission(dict["path"])
+
+        name = eval_dict["name"]
+        description = eval_dict["description"]
+        loc = eval_dict["location"]
+        location = Printf.@sprintf("%.2f˚N, %.2f˚E", loc[2], loc[1])
+        total_precip = Printf.@sprinft("%.3f", eval_dict["total precipitation"])
+        convection_share = Printf.@sprinft("%.1f", 100*eval_dict["convection share"])
+        n_days = Dates.Days(eval_dict["period"]).value
+
+        println(mdfile, "| $name | $description | $location | $total_precip | $convection_share | $n_days |")
+    end
 end
 
 rainmaker_challenge = [
