c530c5c829115d3af5eafbe70613794f36bdb79e
Milan <milankloewer@gmx.de>
2024-10-09 15:44:34 +0100

    Add atlantic mountain submission

--- submissions/atlantic_mountain.jl
+++ submissions/atlantic_mountain.jl
@@ -1,16 +1,15 @@
 author = "Milan"
-description = "Aquaplanet"
+description = "Atlantic mountain"
 
 using SpeedyWeather, RainMaker
 
 spectral_grid = SpectralGrid(trunc=31, nlayers=8)
-
-# define aquaplanet
-ocean = AquaPlanet(spectral_grid, temp_equator=302, temp_poles=273)
-land_sea_mask = AquaPlanetMask(spectral_grid)
-orography = NoOrography(spectral_grid)
 model = PrimitiveWetModel(; spectral_grid, ocean, land_sea_mask, orography)
 model.feedback.verbose = false # hide
+
+# add a massive mountain at 50°N, 35°W
+H, λ₀, φ₀, σ = 4000, 325, 50, 5         # height, lon, lat position, and width
+set!(model, orography=(λ,φ) -> H*exp((-(λ-λ₀)^2 - (φ-φ₀)^2)/2σ^2), add=true)
 
 rain_gauge = RainGauge(spectral_grid, lond=-1.25, latd=51.75)
 add!(model, rain_gauge)
--- submissions/default.jl
+++ submissions/default.jl
@@ -5,6 +5,7 @@ using SpeedyWeather, RainMaker
 
 spectral_grid = SpectralGrid(trunc=31, nlayers=8)
 model = PrimitiveWetModel(; spectral_grid)
+model.feedback.verbose = false # hide
 
 rain_gauge = RainGauge(spectral_grid, lond=-1.25, latd=51.75)
 add!(model, rain_gauge)
