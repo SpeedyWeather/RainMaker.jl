763a26a31cf82986b151e59e342aac50a74774dc
Milan <milankloewer@gmx.de>
2024-10-09 16:33:02 +0100

    add mountain after initialization

--- submissions/atlantic_mountain.jl
+++ submissions/atlantic_mountain.jl
@@ -7,12 +7,13 @@ spectral_grid = SpectralGrid(trunc=31, nlayers=8)
 model = PrimitiveWetModel(; spectral_grid)
 model.feedback.verbose = false # hide
 
-# add a massive mountain at 50°N, 35°W
-H, λ₀, φ₀, σ = 4000, 325, 50, 5         # height, lon, lat position, and width
-set!(model, orography=(λ,φ) -> H*exp((-(λ-λ₀)^2 - (φ-φ₀)^2)/2σ^2), add=true)
-
 rain_gauge = RainGauge(spectral_grid, lond=-1.25, latd=51.75)
 add!(model, rain_gauge)
 
 simulation = initialize!(model)
+
+# add a massive mountain at 50°N, 35°W, *after* model initialization
+H, λ₀, φ₀, σ = 4000, 325, 50, 5     # height, lon, lat position, and width
+set!(model, orography=(λ,φ) -> H*exp((-(λ-λ₀)^2 - (φ-φ₀)^2)/2σ^2), add=true)
+
 run!(simulation, period=Day(20))
