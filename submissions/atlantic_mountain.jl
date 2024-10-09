aff2eb227409e44c5a8790106b55852461c242c2
Milan <milankloewer@gmx.de>
2024-10-09 16:04:54 +0100

    atlantic mountain setup

--- submissions/atlantic_mountain.jl
+++ submissions/atlantic_mountain.jl
@@ -4,7 +4,7 @@ description = "Atlantic mountain"
 using SpeedyWeather, RainMaker
 
 spectral_grid = SpectralGrid(trunc=31, nlayers=8)
-model = PrimitiveWetModel(; spectral_grid, ocean, land_sea_mask, orography)
+model = PrimitiveWetModel(; spectral_grid)
 model.feedback.verbose = false # hide
 
 # add a massive mountain at 50°N, 35°W
