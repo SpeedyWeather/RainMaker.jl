d9f0f511480f21c295b9b247f363ac57e3dae1b1
Milan <milankloewer@gmx.de>
2024-10-28 17:34:40 +0000

    use spherical distance for Tim's submission

--- submissions/north_sea_mountain.jl
+++ submissions/north_sea_mountain.jl
@@ -4,8 +4,7 @@ description = "North Sea mountain"
 using SpeedyWeather, RainMaker
 
 spectral_grid = SpectralGrid(trunc=31, nlayers=8)
-model = PrimitiveWetModel(; spectral_grid)
-model.feedback.verbose = false # hide
+model = PrimitiveWetModel(spectral_grid)
 
 rain_gauge = RainGauge(spectral_grid, lond=-1.25, latd=51.75)
 add!(model, rain_gauge)
@@ -13,7 +12,8 @@ add!(model, rain_gauge)
 simulation = initialize!(model)
 
 # add a massive mountain at 51.75°N, 0°W, *after* model initialization
+# using spherical_distance for geodesic distances, use radius=360/2π for distance in degrees
 H, λ₀, φ₀, σ = 4000, 2, 51, 5     # height, lon, lat position, and width
-set!(model, orography=(λ,φ) -> H*exp((-(λ-λ₀)^2 - (φ-φ₀)^2)/2σ^2), add=true)
+set!(model, orography=(λ,φ) -> H*exp(-spherical_distance((λ,φ), (λ₀,φ₀), radius=360/2π)^2/2σ^2))
 
 run!(simulation, period=Day(20))
