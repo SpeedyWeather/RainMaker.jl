303b4e1daf5e13e7229d8a78c5c85437be6a498e
Tim Reichelt <treigerm@users.noreply.github.com>
2024-10-29 13:24:31 +0000

    Add North Sea submission (#15)
    * Add North Sea submission
    
    * use spherical distance for Tim's submission
    
    * require SpeedyWeather v0.12
    
    ---------
    
    Co-authored-by: Milan <milankloewer@gmx.de>

---
+++ submissions/north_sea_mountain.jl
@@ -0,0 +1,19 @@
+author = "Tim Reichelt"
+description = "North Sea mountain"
+
+using SpeedyWeather, RainMaker
+
+spectral_grid = SpectralGrid(trunc=31, nlayers=8)
+model = PrimitiveWetModel(spectral_grid)
+
+rain_gauge = RainGauge(spectral_grid, lond=-1.25, latd=51.75)
+add!(model, rain_gauge)
+
+simulation = initialize!(model)
+
+# add a massive mountain at 51.75°N, 0°W, *after* model initialization
+# using spherical_distance for geodesic distances, use radius=360/2π for distance in degrees
+H, λ₀, φ₀, σ = 4000, 2, 51, 5     # height, lon, lat position, and width
+set!(model, orography=(λ,φ) -> H*exp(-spherical_distance((λ,φ), (λ₀,φ₀), radius=360/2π)^2/2σ^2))
+
+run!(simulation, period=Day(20))
