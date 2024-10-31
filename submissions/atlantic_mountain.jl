ec876c7ffdbe4373ed675ed3026a30ac9fbc71b7
Milan Klöwer <milankloewer@gmx.de>
2024-10-31 17:29:53 +0000

    spectral_grid as positional argument (#23)
    * rain gauge relative to precip at initialize!
    
    * also sort submissions by rank
    
    * More documentation
    
    * m2mm conversion error
    
    * New to Julia added in docs
    
    * spectral_grid as positional argument

--- submissions/atlantic_mountain.jl
+++ submissions/atlantic_mountain.jl
@@ -4,7 +4,7 @@ description = "Atlantic mountain"
 using SpeedyWeather, RainMaker
 
 spectral_grid = SpectralGrid(trunc=31, nlayers=8)
-model = PrimitiveWetModel(; spectral_grid)
+model = PrimitiveWetModel(spectral_grid)
 
 rain_gauge = RainGauge(spectral_grid, lond=-1.25, latd=51.75)
 add!(model, rain_gauge)
