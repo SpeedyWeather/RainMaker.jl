77848bc8208243f2baad2ded8aac49890f1db88c
Milan <milankloewer@gmx.de>
2024-10-31 16:38:13 +0000

    spectral_grid as positional argument

--- submissions/aquaplanet.jl
+++ submissions/aquaplanet.jl
@@ -9,7 +9,7 @@ spectral_grid = SpectralGrid(trunc=31, nlayers=8)
 ocean = AquaPlanet(spectral_grid, temp_equator=302, temp_poles=273)
 land_sea_mask = AquaPlanetMask(spectral_grid)
 orography = NoOrography(spectral_grid)
-model = PrimitiveWetModel(; spectral_grid, ocean, land_sea_mask, orography)
+model = PrimitiveWetModel(spectral_grid; ocean, land_sea_mask, orography)
 
 rain_gauge = RainGauge(spectral_grid, lond=-1.25, latd=51.75)
 add!(model, rain_gauge)
