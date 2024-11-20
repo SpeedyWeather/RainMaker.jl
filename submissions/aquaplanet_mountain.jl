5d34ebd6901bdad30c5b20d0f1404129ecb17f92
Milan Klöwer <milankloewer@gmx.de>
2024-11-20 21:41:29 +0000

    Move mountain set! to after initialization

--- submissions/aquaplanet_mountain.jl
+++ submissions/aquaplanet_mountain.jl
@@ -6,15 +6,10 @@ using SpeedyWeather, RainMaker
 spectral_grid = SpectralGrid(trunc=31, nlayers=10)
 model = PrimitiveWetModel(spectral_grid)
 
-# Details for mountain
-H, λ₀, φ₀, σ = 4000, 2, 51, 5     # height, lon, lat position, and width
-
-# Set up aqauaplanet with large mountain in "North Sea"
+# Set up aqauaplanet but add large mountain in "North Sea" after initialization!
 ocean = AquaPlanet(spectral_grid, temp_equator=302, temp_poles=300)
-orography = NoOrography(spectral_grid)
-# land_sea_mask = AquaPlanetMask(spectral_grid)
-model = PrimitiveWetModel(spectral_grid; ocean, orography)
-set!(model, orography=(λ,φ) -> H*exp(-spherical_distance((λ,φ), (λ₀,φ₀), radius=360/2π)^2/2σ^2))
+land_sea_mask = AquaPlanetMask(spectral_grid)
+model = PrimitiveWetModel(spectral_grid; ocean, land_sea_mask)
 
 # Add rain gauge
 rain_gauge = RainGauge(spectral_grid, lond=-1.25, latd=51.75)
@@ -23,10 +18,14 @@ add!(model, rain_gauge)
 # Initialize and run simulation
 simulation = initialize!(model, time=DateTime(2000, 9, 1))
 
+# Add mountain now! details for mountain
+H, λ₀, φ₀, σ = 4000, 2, 51, 5     # height, lon, lat position, and width
+set!(model, orography=(λ,φ) -> H*exp(-spherical_distance((λ,φ), (λ₀,φ₀), radius=360/2π)^2/2σ^2))
+
 # Run simulation for 20 days
 run!(simulation, period=Day(20))
 
-# Plot the results
+# Plot the results (not needed for submission but doesn't hurt!)
 using CairoMakie
 # heatmap(model.orography.orography, title="My orogoraphy [m]") # check orography
 RainMaker.plot(rain_gauge, rate_Δt=Hour(1))
