cc2101585dfa7ef52158dc0eaf8c9d5783496439
Maren Höver <68069103+marenhoever@users.noreply.github.com>
2024-11-21 16:31:42 +0000

    First try (#32)
    * First try
    
    * changing humidity
    
    * Removing unnecessary line

---
+++ submissions/maren.jl
@@ -0,0 +1,19 @@
+author = "Maren"
+description = "Hot aqua planet with SST anomalie"
+
+using SpeedyWeather, RainMaker
+using CairoMakie
+
+# create a model
+spectral_grid = SpectralGrid(trunc=31, nlayers=8)
+ocean = AquaPlanet(spectral_grid, temp_equator = 300.0, temp_poles= 300.0)
+model = PrimitiveWetModel(spectral_grid; ocean)
+
+# add the rain gauge as callback
+rain_gauge = RainGauge(spectral_grid, lond=-1.25, latd=51.75)
+add!(model, rain_gauge)
+
+simulation = initialize!(model)
+set!(simulation, humid=0.18)
+set!(simulation, sea_surface_temperature=(λ, φ) -> (50 < φ < 53) && (50 < λ < 60) ? 5 : 0, add=true)
+run!(simulation, period=Day(20))
