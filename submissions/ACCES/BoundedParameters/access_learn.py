f6b64bee30e5d05a8ce9b2a18ae647b767193fdd
anicusan <aln705@student.bham.ac.uk>
2025-07-25 08:35:53 -0400

    added ACCES best parameters into individual scripts

--- submissions/ACCES/BoundedParameters/access_learn.py
+++
@@ -1,6 +0,0 @@
-import coexist
-coexist.Access("simulation_script.py").learn(
-    num_solutions=10,
-    target_sigma=0.01,
-    random_seed=42,
-)
---
+++ submissions/anicusan_evoopt_bounded.jl
@@ -0,0 +1,18 @@
+using RainMakerChallenge2025
+
+best_params = [
+    1.999196,
+    2304.509728,
+    14.889662,
+    74.446324,
+    -70.338707,
+    280.224610,
+    299.998274,
+    4.999593,
+    4.996833,
+    33.312800,
+]
+precipitation = max_precipitation(best_params)
+
+println("Maximum precipitation (mm):")
+println(precipitation)
